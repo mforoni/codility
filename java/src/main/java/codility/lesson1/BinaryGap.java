a515fa7c6265ad3ae5f4d93165248100b71d81f4
mforoni <marcofor10@gmail.com>
2018-08-12 21:05:45 +0200

    fix javadoc

--- java/src/main/java/codility/lesson1/BinaryGap.java
+++ java/src/main/java/codility/lesson1/BinaryGap.java
@@ -4,41 +4,42 @@ package codility.lesson1;
  * A binary gap within a positive integer N is any maximal sequence of
  * consecutive zeros that is surrounded by ones at both ends in the binary
  * representation of N.
- * 
+ * <p>
  * For example, number 9 has binary representation 1001 and contains a binary
  * gap of length 2. The number 529 has binary representation 1000010001 and
  * contains two binary gaps: one of length 4 and one of length 3. The number 20
  * has binary representation 10100 and contains one binary gap of length 1. The
  * number 15 has binary representation 1111 and has no binary gaps. The number
  * 32 has binary representation 100000 and has no binary gaps.
- * 
+ * <p>
  * Write a function:
- * 
+ * <p>
  * class Solution { public int solution(int N); }
- * 
+ * <p>
  * that, given a positive integer N, returns the length of its longest binary
  * gap. The function should return 0 if N doesn't contain a binary gap.
- * 
+ * <p>
  * For example, given N = 1041 the function should return 5, because N has
  * binary representation 10000010001 and so its longest binary gap is of length
  * 5. Given N = 32 the function should return 0, because N has binary
  * representation '100000' and thus no binary gaps.
- * 
+ * <p>
  * Assume that:
+ * <p>
+ * N is an integer within the range [1..2,147,483,647].
+ * <p>
+ * Complexity:
+ * <p>
+ * expected worst-case time complexity is O(log(N));<br>
+ * expected worst-case space complexity is O(1).
  * 
- * N is an integer within the range [1..2,147,483,647]. Complexity:
- * 
- * expected worst-case time complexity is O(log(N)); expected worst-case space
- * complexity is O(1).
- * 
- * @author marco
+ * @author Foroni Marco
  *
  */
 final class BinaryGap {
 
 	/**
-	 * Time complexity should be O(logN)
-	 * Space complexity is O(1)
+	 * Time complexity should be O(logN) Space complexity is O(1)
 	 * 
 	 * @param N
 	 * @return
@@ -61,7 +62,7 @@ final class BinaryGap {
 		}
 		return max;
 	}
-	
+
 	public static int lastIndexOf(final String s, final char c) {
 		for (int i = s.length() - 1; i >= 0; i--) {
 			if (s.charAt(i) == c) {
