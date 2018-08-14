4b065f15ae4e2286ee5a7a576d61fb027b7dd861
mforoni <marcofor10@gmail.com>
2018-08-14 12:45:22 +0200

    reduce massive test number

--- java/src/test/java/codility/lesson5/GenomicRangeQueryTest.java
+++ java/src/test/java/codility/lesson5/GenomicRangeQueryTest.java
@@ -9,9 +9,9 @@ import codility.util.MoreInts;
 public class GenomicRangeQueryTest {
 
 	private static final int MIN_N = 1;
-	private static final int MAX_N = 100_000;
+	private static final int MAX_N = 10_000; // original = 100_000;
 	private static final int MIN_M = 1;
-	private static final int MAX_M = 50_000;
+	private static final int MAX_M = 5_000; // original = 50_000;
 
 	private static final String S1 = "CAGCCTA";
 	private static final int[] P1 = { 2, 5, 0 };
@@ -36,13 +36,11 @@ public class GenomicRangeQueryTest {
 	@Test
 	public void testMassive() {
 		final GenomicRangeQuery genomicRangeQuery = new GenomicRangeQuery();
-		int ntest = 200;
+		int ntest = 100;
 		for (int t = 0; t < ntest; t++) {
-//			final int n = MoreInts.newRandom(MIN_N, MAX_N);
-			final int n = MoreInts.newRandom(MIN_N, 10_000);
+			final int n = MoreInts.newRandom(MIN_N, MAX_N);
 			final int[] s = MoreInts.newRandomArray(n, 1, 4);
-//			final int m = MoreInts.newRandom(MIN_M, MAX_M);
-			final int m = MoreInts.newRandom(MIN_M, 5_000);
+			final int m = MoreInts.newRandom(MIN_M, MAX_M);
 			final int[] P = new int[m];
 			final int[] Q = new int[m];
 			for (int i = 0; i < m; i++) {
