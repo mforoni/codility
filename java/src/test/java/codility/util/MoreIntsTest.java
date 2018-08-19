4e228f138a41ee25eab6f7af807d80a1952d542c
mforoni <marcofor10@gmail.com>
2018-08-19 21:57:47 +0200

    add unit test for MoreInts

---
+++ java/src/test/java/codility/util/MoreIntsTest.java
@@ -0,0 +1,93 @@
+package codility.util;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MoreIntsTest {
+
+	private static final int[] A1 = { 3, 2, -6, 4, 0 };
+
+	@Test
+	public void testNewRandom() {
+	}
+
+	@Test
+	public void testNewRandomArray() {
+	}
+
+	@Test
+	public void testPrefixSums() {
+		assertArrayEquals(new int[] { 3, 5, -1, 3, 3 }, MoreInts.prefixSums(A1));
+	}
+
+	@Test
+	public void testSum() {
+		assertEquals(0, MoreInts.sum(A1, 1, 3));
+	}
+
+	@Test
+	public void testMinIntArray() {
+		assertEquals(-6, MoreInts.min(A1));
+	}
+
+	@Test
+	public void testMinIntArrayIntInt() {
+		assertEquals(2, MoreInts.min(A1, 0, 1));
+	}
+
+	@Test
+	public void testMinIndexIntArray() {
+		assertEquals(2, MoreInts.minIndex(A1));
+	}
+
+	@Test
+	public void testMinIndexIntArrayIntInt() {
+		assertEquals(4, MoreInts.minIndex(A1, 3, 4));
+	}
+
+	@Test
+	public void testMaxIntArray() {
+		assertEquals(4, MoreInts.max(A1));
+	}
+
+	@Test
+	public void testMaxIntArrayIntInt() {
+	}
+
+	@Test
+	public void testMaxIndexIntArray() {
+	}
+
+	@Test
+	public void testMaxIndexIntArrayIntInt() {
+	}
+
+	@Test
+	public void testMaxIntIntInt() {
+	}
+
+	@Test
+	public void testIndexOfIntArrayInt() {
+	}
+
+	@Test
+	public void testIndexOfIntArrayIntInt() {
+	}
+
+	@Test
+	public void testIndexOfIntArrayIntIntInt() {
+	}
+
+	@Test
+	public void testSwap() {
+
+	}
+
+	@Test
+	public void testToStringIntArray() {
+		assertEquals("3,2,-6,4,0", MoreInts.toString(A1));
+	}
+
+}
